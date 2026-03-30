4cc6b81d93d0e120b8209ecaced88acb6c95e31f
fixbro.in <djdjapps@gmail.com>
2026-03-30 12:21:09 +0530

    update project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,7 +94,7 @@ export async function generateViewport(): Promise<Viewport> {
     initialScale: 1,
     maximumScale: 5,
     userScalable: true,
-    interactiveWidget: 'resizes-content',
+    interactiveWidget: 'overlays-content',
   };
 }
 
